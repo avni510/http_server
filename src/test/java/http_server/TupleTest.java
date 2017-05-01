85022afc346d73bd166da5e86ac37ea5f171802e
Avni Kothari <avnikothari@Avnis-MacBook-Air.local>
2017-05-01 13:26:35 -0500

    Created a Router
    * Router handles generating the appropiate http response for a given
    request
    * Can easily add new routes
    * Created a Tuple class to store as keys for the Router

---
+++ src/test/java/http_server/TupleTest.java
@@ -0,0 +1,92 @@
+package http_server;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class TupleTest {
+
+  @Test
+  public void firstElementIsReturned() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple = new Tuple<>(RequestMethod.GET, "/");
+
+    Enum<RequestMethod> actualResult = tuple.getFirstElement();
+
+    assertEquals(RequestMethod.GET, actualResult);
+  }
+
+  @Test
+  public void secondElementIsReturned() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple = new Tuple<>(RequestMethod.GET, "/");
+
+    String actualResult = tuple.getSecondElement();
+
+    assertEquals("/", actualResult);
+  }
+
+  @Test
+  public void twoTuplesWithTheSameValuesAreEqual() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple1 = new Tuple<>(RequestMethod.GET, "/");
+    Tuple<Enum<RequestMethod>, String> tuple2 = new Tuple<>(RequestMethod.GET, "/");
+
+    boolean actualResult = tuple1.equals(tuple2);
+
+    assertTrue(actualResult);
+  }
+
+  @Test
+  public void equalityIsSymmetric() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple1 = new Tuple<>(RequestMethod.GET, "/");
+    Tuple<Enum<RequestMethod>, String> tuple2 = new Tuple<>(RequestMethod.GET, "/");
+
+    boolean tuple1EqualsTuple2 = tuple1.equals(tuple2);
+    boolean tuple2EqualsTuple1 = tuple2.equals(tuple1);
+
+    assertEquals(tuple1EqualsTuple2, tuple2EqualsTuple1);
+  }
+
+  @Test
+  public void theSameTupleIsEqual() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple1 = new Tuple<>(RequestMethod.GET, "/");
+
+    boolean actualResult = tuple1.equals(tuple1);
+
+    assertTrue(actualResult);
+  }
+
+  @Test
+  public void twoTuplesWithDifferentValuesAreNotEqual() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple1 = new Tuple<>(RequestMethod.GET, "/");
+    Tuple<Enum<RequestMethod>, String> tuple2 = new Tuple<>(RequestMethod.GET, "/code");
+
+    boolean actualResult = tuple1.equals(tuple2);
+
+    assertFalse(actualResult);
+  }
+
+  @Test
+  public void twoDifferentObjectsAreNotEqual() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple = new Tuple<>(RequestMethod.GET, "/");
+    Map<String, String> hashMap = new HashMap<>();
+
+    boolean actualResult = tuple.equals(hashMap);
+
+    assertFalse(actualResult);
+  }
+
+  @Test
+  public void equalTuplesProduceTheSameHashCode() throws Exception {
+    Tuple<Enum<RequestMethod>, String> tuple1 = new Tuple<>(RequestMethod.GET, "/");
+    Tuple<Enum<RequestMethod>, String> tuple2 = new Tuple<>(RequestMethod.GET, "/");
+    
+    if (tuple1.equals(tuple2)) {
+      assertTrue(tuple1.hashCode() == tuple2.hashCode());
+    }
+    else {
+      System.out.println("tuples are not equal");
+    }
+  }
+}
